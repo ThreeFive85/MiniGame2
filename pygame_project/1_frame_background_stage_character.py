d97ab0fa0c7acfcd5c20cc121412c6b0f8654db9
박동혁 <hyuk@MacBook-Pro.local>
2020-06-25 13:57:30 +0900

    make character

--- pygame_project/1_frame_background_stage_character.py
+++ pygame_project/1_frame_background_stage_character.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-os.environ['SDL_VIDEODRIVER'] = "dummy"
+# os.environ['SDL_VIDEODRIVER'] = "dummy"
 ##################################################################
 # 기본 초기화 (반드시 해야 하는 것들)
 pygame.init()  # 초기화 (반드시 필요)
@@ -22,12 +22,20 @@ current_path = os.path.dirname(__file__)  # 현재 파일의 위치 반환
 image_path = os.path.join(current_path, "images")  # images 폴더 위치 반환
 
 # 배경 만들기
-background = os.path.join(image_path, "background.png")
+background = pygame.image.load(os.path.join(image_path, "background.png"))
 
 # 스테이지 만들기
-stage = os.path.join(image_path, "stage.png")
+stage = pygame.image.load(os.path.join(image_path, "stage.png"))
 stage_size = stage.get_rect().size
 stage_height = stage_size[1]  # 스테이지의 높이 위에 캐릭터를 두기 위해 사용
+
+# 캐릭터 만들기
+character = pygame.image.load(os.path.join(image_path, "character.png"))
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width / 2)
+character_y_pos = screen_height - character_height - stage_height
 
 
 running = True  # 게임이 진행중인가?
@@ -46,6 +54,7 @@ while running:
     # 5. 화면에 그리기
     screen.blit(background, (0, 0))
     screen.blit(stage, (0, screen_height - stage_height))
+    screen.blit(character, (character_x_pos, character_y_pos))
 
     pygame.display.update()  # 게임 화면을 다시 그리기
 
